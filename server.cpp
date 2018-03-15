7fd0b2f24b3801b95a935598de584511587412b5
Brandon Baars <baarsb@eos12.cis.gvsu.edu>
2018-03-15 14:03:50 -0400

    First Commit

---
+++ server.cpp
@@ -0,0 +1,27 @@
+/*
+ * @authors: Brandon Baars, Mike Ford, Isaac Benson
+ * @date: 03/15/2018
+ * @description: CIS 457 Project 3: TCP Encrytped Chat Server
+ *
+ */
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string>
+#include <pthread.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <signal.h>
+#include <map>
+
+
+int main(int argc, char** argv) {
+
+
+
+
+
+
+
+}
