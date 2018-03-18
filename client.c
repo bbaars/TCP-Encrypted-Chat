2bdbc49924dc1f0f7f9f059bf19828b26ec97043
Brandon Baars <baarsb@eos12.cis.gvsu.edu>
2018-03-18 14:18:49 -0400

    Added admin kicking

---
+++ client.c
@@ -0,0 +1,111 @@
+/*
+* @authors: Brandon Baars, Mike Ford, Isaac Benson
+* @date: 03/15/2018
+* @description: CIS 457 Project 3: TCP Encrytped Chat Client
+*
+*/
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, char** argv)
+{
+	bool run = true;
+	int sockfd = socket(AF_INET,SOCK_STREAM,0);
+	int port = 0;
+	int i;
+	fd_set sockets;
+	FD_ZERO(&sockets);
+
+	/*Attempt to set up Socket*/
+	if(sockfd<0)
+	{
+		printf("There was an error creating the socket\n");
+		return 1;
+	}
+
+	printf("--------------Welcome to the TCP Encrypted Chat Client--------------"
+	"\nCOMMAND:                    DESCRIPTION:"
+	"\n-m [client_id] [message]    send a message to a client"
+	"\n-b [message]                send a broadcast message"
+	"\n-l                          retrieve a list of all clients"
+	"\n-k [client_id]              kick off another client (admin req.)"
+	"\n-q                          Quit"
+	"\n--------------------------------------------------------------------\n"
+);
+
+/*Take input for port number*/
+printf("\nEnter in a port number:   ");
+scanf("%d", &port);
+
+/*Remove newline character that results from calling scanf()*/
+getchar();
+
+/*Set up connection to server*/
+struct sockaddr_in serveraddr;
+serveraddr.sin_family=AF_INET;
+serveraddr.sin_port=htons(port);
+serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+
+/*Attempt to connect to server*/
+int e = connect(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+if(e<0)
+{
+	printf("There was an error connecting\n");
+	return 1;
+}
+
+/*Prompt to enter a command*/
+printf("Enter a command: ");
+fflush(stdout);
+
+/*Set stdin and socket select bits*/
+FD_SET (0, &sockets);
+FD_SET (sockfd, &sockets);
+while (run)
+{
+
+	fd_set tmp_set = sockets;
+	select(FD_SETSIZE, &tmp_set, NULL, NULL, NULL);
+
+	/*Iterate through select bits*/
+	for (i = 0; i < FD_SETSIZE; i++)
+	{
+		if(FD_ISSET(i, &tmp_set))
+		{
+			/*If user is typing into the console*/
+			if (i == 0)
+			{
+				printf("Enter a command: ");
+				char line[5000];
+				fgets(line, 5000, stdin);
+				send(sockfd,line,strlen(line)+1,0);
+			}
+
+			/*If the server is sending something to the client*/
+			else if (i == sockfd)
+			{
+				char line[5000];
+				recv(sockfd,line,5000,0);
+
+				/*If quit message was received, exit*/
+				if (strstr(line, "disconnected from server") != NULL)
+				{
+					run = false;
+					printf("Disconnected from servers\n"); //Newline
+					break;
+				}
+				printf("\nGot from server: %s\nEnter a command:",line);
+				fflush(stdout);
+			}
+		}
+	}
+}
+
+close(sockfd);
+return 0;
+
+}
--- server.cpp
+++ server.cpp
@@ -1,9 +1,9 @@
 /*
- * @authors: Brandon Baars, Mike Ford, Isaac Benson
- * @date: 03/15/2018
- * @description: CIS 457 Project 3: TCP Encrytped Chat Server
- *
- */
+* @authors: Brandon Baars, Mike Ford, Isaac Benson
+* @date: 03/15/2018
+* @description: CIS 457 Project 3: TCP Encrytped Chat Server
+*
+*/
 
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -33,253 +33,253 @@ void broadcast(struct client_info client);
 // Struct that handles our clients request.
 struct client_info {
 
-								int from_socket; // socket the request was from
-								uint32_t ipaddr; // ip address of the client
-								std::vector<int> to_socket; // vector to hold the sockets to send to (broadcast or 1)
-								std::string message; // message the client wants to send
-								bool is_admin; // whether or not the client has admin access
-								pthread_t id; // thread id that this client is attached to
-								std::string username; // the username of the client (used to map itself to the map?)
+	int from_socket; 					// socket the request was from
+	uint32_t ipaddr; 					// ip address of the client
+	std::vector<int> to_socket; 		// vector to hold the sockets to send to (broadcast or 1)
+	std::string message; 				// message the client wants to send
+	bool is_admin; 						// whether or not the client has admin access
+	pthread_t id; 						// thread id that this client is attached to
+	std::string username; 				// the username of the client (used to map itself to the map?)
 };
 
 // list of all the connected clients
 std::map<std::string, struct client_info> connected_clients;
 
 //Global password
-std::string PASSWORD = "password";
-
+std::string PASSWORD = "password\n";
 
 /*
- *	Handle the connection received with the client
- */
+*	Handle the connection received with the client
+*/
 void * handle_receive(void * client_arg) {
 
-								struct client_info client = *(struct client_info*) client_arg;
-								struct in_addr ipaddr;
-								ipaddr.s_addr = client.ipaddr;
+	struct client_info client = *(struct client_info*) client_arg;
+	struct in_addr ipaddr;
+	ipaddr.s_addr = client.ipaddr;
 
-								std::cout << "Received client: " << client.from_socket << " from IP Addr: "
-																		<< inet_ntoa(ipaddr) << std::endl;
+	std::cout << "Received client: " << client.from_socket << " from IP Addr: "
+	<< inet_ntoa(ipaddr) << std::endl;
 
-								// while there is still an active connection with the client
-								bool is_running = true;
+	// while there is still an active connection with the client
+	bool is_running = true;
 
-								client.id = pthread_self();
+	client.id = pthread_self();
 
-								while(is_running) {
+	while(is_running) {
 
-																char line[5000];
-																int rec = recv(client.from_socket, line, 5000, 0);
+		char line[5000];
+		int rec = recv(client.from_socket, line, 5000, 0);
 
-																std::string str_line(line);
-																client.message = str_line;
+		std::string str_line(line);
+		client.message = str_line;
 
-																//if there was a message received with more than a command (i.e 2 characters -L)
-																if (rec >= 2) {
-																								parse_message(client);
-																}
-								}
+		//if there was a message received with more than a command (i.e 2 characters -L)
+		if (rec >= 2) {
+			parse_message(client);
+		}
+	}
+
+	return NULL;
 }
 
 
 void parse_message(struct client_info & client) {
 
-								// There was an argument given
-								if(client.message.find("-") == 0) {
+	// There was an argument given
+	if(client.message.find("-") == 0) {
 
-																// make sure we don't get index out of bounds
-																if (client.message.length() >= 2) {
+		// make sure we don't get index out of bounds
+		if (client.message.length() >= 2) {
 
-																								// get the command issued by the client
-																								char command = client.message.at(1);
-																								std::string id;
+			// get the command issued by the client
+			char command = client.message.at(1);
+			std::string id;
 
-																								char error[] = "Sorry, there was an error";
-																								char passwordError[] = "Incorrect Password.";
-																								switch(command) {
+			char error[] = "Sorry, there was an error";
+			char passwordError[] = "Incorrect Password.";
+			switch(command) {
 
-																								case 'L':
-																								case 'l':
-																																send_list(client);
-																																break;
-																								case 'M':
-																								case 'm':
-																																// parse for recipient id
-																																// send client message
-																																// TODO: Reformat string to include who it's from
-																																id = parse_id(client.message);
-																																if (!id.empty() && client.message.length() > 5) {
-																																								// send to other client the message - the command and id
-																																								send(connected_clients[id].from_socket,
-																																													client.message.substr(client.message.find(id) + 1).c_str(),
-																																													strlen(client.message.substr(client.message.find(id)).c_str()), 0);
-																																} else {
-																																								// Send messge back to client saying can't send message to self
-																																								send(client.from_socket, error, strlen(error) + 1, 0);
-																																}
+			case 'L':
+			case 'l':
+				send_list(client);
+				break;
+			case 'M':
+			case 'm':
+				// parse for recipient id
+				// send client message
+				// TODO: Reformat string to include who it's from
+				id = parse_id(client.message);
+				if (!id.empty() && client.message.length() > 5) {
+					// send to other client the message - the command and id
+					send(connected_clients[id].from_socket,
+					client.message.substr(client.message.find(id) + 1).c_str(),
+					strlen(client.message.substr(client.message.find(id)).c_str()), 0);
+				} else {
+					// Send messge back to client saying can't send message to self
+					send(client.from_socket, error, strlen(error) + 1, 0);
+				}
 
-																																break;
-																								case 'B':
-																								case 'b':
-																																// broadcast messagee
-																																broadcast(client);
-																																break;
-																								case 'k':
-																								case 'K':
-																																// parse for recipient id
-																																// Kill user
+				break;
+			case 'B':
+			case 'b':
+				// broadcast messagee
+				broadcast(client);
+				break;
+			case 'k':
+			case 'K':
+				// check is the user is an admin, and the person they want to kick is NOTs
+				if (client.is_admin) {
+					if(!connected_clients[parse_id(client.message)].is_admin) {
+						terminate_user(connected_clients[parse_id(client.message)]);
+					} else {
+						send(client.from_socket, "You can't kick an admin\n", strlen("You can't kick an admin\n") + 1, 0);
+					}
+				} else
+					send(client.from_socket, "You are not an admin\n", strlen("You are not an admin\n") + 1, 0);
+				break;
+			case 'q':
+			case 'Q':
+				// close the thread and remove client from active list
+				terminate_user(client);
+				break;
 
-																																break;
-																								case 'q':
-																								case 'Q':
-																																// close the thread and remove client from active list
-																																terminate_user(client);
-																																break;
+			case 'a':
+			case 'A':
+				if(client.message.substr(client.message.find("-")+3).compare(PASSWORD) == 0) {
+					client.is_admin = true;
+					send(client.from_socket, "You are now an admin\n", strlen("You are now an admin\n") + 1, 0);
+				} else
+					send(client.from_socket, passwordError, strlen(passwordError) + 1, 0);
+				break;
 
-																								case 'a':
-																								case 'A':
-																																if(client.message.substr(client.message.find("-")+3).compare(PASSWORD) == 0)
-																																								client.is_admin = true;
-																																else
-																																{
-																																								send(client.from_socket, passwordError, strlen(passwordError) + 1, 0);
-																																}
+			default:
+				// no command was given
+				// TODO: send error message back
+				break;
+			}
 
-																																break;
-
-
-																								default:
-																																// no command was given
-																																// TODO: send error message back
-																																break;
-																								}
-
-																} else {
-																								// TODO: Send error message back
-																}
-								}
+		} else {
+			// TODO: Send error message back
+		}
+	}
 }
 
 void broadcast(struct client_info client) {
 
-								// loop through our connected clients and create a string of connected clients id's
-								for(std::map<std::string, struct client_info>::iterator it = connected_clients.begin();
-												it != connected_clients.end(); ++it) {
+	// loop through our connected clients and create a string of connected clients id's
+	for(std::map<std::string, struct client_info>::iterator it = connected_clients.begin();
+	it != connected_clients.end(); ++it) {
 
-																// Check if it's us, otherwise say it's a client
-																if (client.from_socket != it->second.from_socket) {
-																								send(it->second.from_socket, client.message.substr(3).c_str(), strlen(client.message.c_str()) + 1, 0);
-																}
-								}
+		// Check if it's us, otherwise say it's a client
+		if (client.from_socket != it->second.from_socket) {
+			send(it->second.from_socket, client.message.substr(3).c_str(), strlen(client.message.c_str()) + 1, 0);
+		}
+	}
 }
 
 void send_list(struct client_info client) {
 
-								char message[500];
-								std::string str_message;
+	char message[500];
+	std::string str_message;
 
-								str_message.append("\n");
+	str_message.append("\n");
 
-								// loop through our connected clients and create a string of connected clients id's
-								for(std::map<std::string, struct client_info>::iterator it = connected_clients.begin();
-												it != connected_clients.end(); ++it) {
+	// loop through our connected clients and create a string of connected clients id's
+	for(std::map<std::string, struct client_info>::iterator it = connected_clients.begin();
+	it != connected_clients.end(); ++it) {
 
-																// Check if it's us, otherwise say it's a client
-																if (client.from_socket == it->second.from_socket) {
-																								str_message.append(it->first + ": You\n");
-																} else {
-																								str_message.append(it->first + ": Client\n");
-																}
-								}
+		// Check if it's us, otherwise say it's a client
+		if (client.from_socket == it->second.from_socket) {
+			str_message.append(it->first + ": You\n");
+		} else {
+			str_message.append(it->first + ": Client\n");
+		}
+	}
 
-								strcpy(message, str_message.c_str());
-								send(client.from_socket, message, strlen(message) + 1, 0);
+	strcpy(message, str_message.c_str());
+	send(client.from_socket, message, strlen(message) + 1, 0);
 }
 
 
 std::string parse_id(std::string message) {
 
-								std::string id;
+	std::string id;
 
-								// loop through our connected clients to see if any of the ids match the requested id
-								for(std::map<std::string, struct client_info>::iterator it = connected_clients.begin();
-												it != connected_clients.end(); ++it) {
+	// loop through our connected clients to see if any of the ids match the requested id
+	for(std::map<std::string, struct client_info>::iterator it = connected_clients.begin();
+	it != connected_clients.end(); ++it) {
+		if(message.find(it->first) == 3) {
+			std::cout << "Request for client: " << it->first << std::endl;
+			id = it->first;
+		}
+	}
 
-																if(message.find(it->first) == 3) {
-																								std::cout << "Request for client: " << it->first << std::endl;
-																								id = it->first;
-																}
-								}
-
-								return id;
+	return id;
 }
 
 void terminate_user(struct client_info client) {
 
-								char terminating[] = "You are not disconnected from server..\n";
+	char terminating[] = "You are now disconnected from server..\n";
 
-								send(client.from_socket, terminating, strlen(terminating) + 1, 0);
+	send(client.from_socket, terminating, strlen(terminating) + 1, 0);
 
-								// remove the client from the list of connected clients
-								connected_clients.erase(client.username);
-								pthread_exit(NULL);
+	// remove the client from the list of connected clients
+	connected_clients.erase(client.username);
 }
 
 
 int main(int argc, char** argv) {
 
-								// port to listen on
-								int port;
+	// port to listen on
+	int port;
 
-								// Create a TCP Socket
-								int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-								int total_connected = 0;
+	// Create a TCP Socket
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-								struct sockaddr_in serveraddr, clientaddr;
-								serveraddr.sin_family = AF_INET;
-								serveraddr.sin_addr.s_addr = INADDR_ANY;
+	struct sockaddr_in serveraddr, clientaddr;
+	serveraddr.sin_family = AF_INET;
+	serveraddr.sin_addr.s_addr = INADDR_ANY;
 
-								std::cout << "Enter a port to listen on: ";
-								std::cin >> port;
-								std::cout << "\nListening on port " << port << std::endl;
+	std::cout << "Enter a port to listen on: ";
+	std::cin >> port;
+	std::cout << "\nListening on port " << port << std::endl;
 
-								serveraddr.sin_port = htons(port);
+	serveraddr.sin_port = htons(port);
 
-								bind(sockfd, (struct sockaddr*) &serveraddr, sizeof(serveraddr));
-								listen(sockfd, 10);
+	bind(sockfd, (struct sockaddr*) &serveraddr, sizeof(serveraddr));
+	listen(sockfd, 10);
 
-								// our sending and receivng threads
-								pthread_t receiver, sender;
-
-
-								while(true) {
-
-																socklen_t len = sizeof(clientaddr);
-																int clientsocket = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
-
-																struct client_info client;
-																client.from_socket = clientsocket;
-																client.ipaddr = clientaddr.sin_addr.s_addr;
-
-																// convert our int to a string
-																std::stringstream ss;
-																ss << total_connected;
-
-																// add our client to our map to map client id to client info
-																connected_clients[std::string(ss.str())] = client;
-																connected_clients[std::string(ss.str())].username = std::string(ss.str());
-
-																// pass in our map's client_info struct
-																pthread_create(&receiver, NULL, handle_receive, &connected_clients[std::string(ss.str())]);
-																pthread_detach(receiver);
-																total_connected++;
-								}
-
-								pthread_cancel(receiver);
-								pthread_join(receiver, NULL);
+	// our sending and receivng threads
+	pthread_t receiver;
 
 
-								close(sockfd);
+	while(true) {
 
-								return 0;
+		socklen_t len = sizeof(clientaddr);
+		int clientsocket = accept(sockfd, (struct sockaddr*)&clientaddr, &len);
+
+		struct client_info client;
+		client.from_socket = clientsocket;
+		client.ipaddr = clientaddr.sin_addr.s_addr;
+		client.is_admin = false;
+
+		// convert our int to a string
+		std::stringstream ss;
+		ss << clientsocket;
+
+		// add our client to our map to map client id to client info
+		connected_clients[std::string(ss.str())] = client;
+		connected_clients[std::string(ss.str())].username = std::string(ss.str());
+
+		// pass in our map's client_info struct, detach it
+		pthread_create(&receiver, NULL, handle_receive, &connected_clients[std::string(ss.str())]);
+		pthread_detach(receiver);
+	}
+
+	pthread_cancel(receiver);
+	pthread_join(receiver, NULL);
+
+	close(sockfd);
+
+	return 0;
 }
